e0e7735e1818956a9f3db43ad13ecebaa28bd461
victor_ferreira <vitorsantosferreira10@gmail.com>
2024-12-01 15:18:20 -0300

    first commit

---
+++ internal/service/category.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+	"gRPC/internal/database"
+	"gRPC/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return categoryResponse, nil
+}
